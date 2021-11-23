4081d769eb733342fff91f4833d3959c85be7d53
Arsh Sharma <sharmaa64@student.douglascollege.ca>
2021-11-23 12:47:41 +0530

    Firebase Authentication Successful

--- app/src/main/java/com/example/f21g4_minions/RegistrationActivity.java
+++ app/src/main/java/com/example/f21g4_minions/RegistrationActivity.java
@@ -3,85 +3,79 @@ package com.example.f21g4_minions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    final String TAG_ERROR_REGISTER = "";
+    final String TAG_DEBUG_REGISTER = "";
+
     EditText name, email, password;
     private FirebaseAuth auth;
+    Button btnsignUpRegister;
+    TextView txtsignInRegister;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
 
         auth = FirebaseAuth.getInstance();
 
         name = findViewById(R.id.name);
-        name = findViewById(R.id.email);
+        email = findViewById(R.id.email);
         password = findViewById(R.id.password);
 
+        btnsignUpRegister = findViewById(R.id.signUpRegister);
+        txtsignInRegister = findViewById(R.id.signInRegister);
 
-    }
-
-
-    public void signUp(View view){
-        startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
-
-
-        String userName = name.getText().toString();
-        String userEmail = email.getText().toString();
-        String userPassword = password.getText().toString();
+        // listener on button
+        btnsignUpRegister.setOnClickListener((View view) -> {
+            String userName = name.getText().toString();
+            String userEmail = email.getText().toString();
+            String userPassword = password.getText().toString();
 
 
-        if(TextUtils.isEmpty(userName)){
-            Toast.makeText(this, "Enter Name!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if(TextUtils.isEmpty(userEmail)){
-            Toast.makeText(this, "Enter Email!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if(TextUtils.isEmpty(userPassword)){
-            Toast.makeText(this, "Enter Password!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        if(userPassword.length() < 7){
-            Toast.makeText(this, "Password too short, enter minimum 7 characters!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        auth.createUserWithEmailAndPassword(userEmail,userPassword).addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(RegistrationActivity.this, "Successfully Registered", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
-                }
-                else{
-                    Toast.makeText(RegistrationActivity.this, "Registration Unsuccessful" + task.getException(), Toast.LENGTH_SHORT).show();
-
-                }
+            if(TextUtils.isEmpty(userName)){
+                Toast.makeText(this, "Enter Name!", Toast.LENGTH_SHORT).show();
+            }else if(TextUtils.isEmpty(userEmail)){
+                Toast.makeText(this, "Enter Email!", Toast.LENGTH_SHORT).show();
+            }else if(TextUtils.isEmpty(userPassword)){
+                Toast.makeText(this, "Enter Password!", Toast.LENGTH_SHORT).show();
+            }else if(userPassword.length() < 7){
+                Toast.makeText(this, "Password too short, enter minimum 7 characters!", Toast.LENGTH_SHORT).show();
+            }else{
+                auth.createUserWithEmailAndPassword(userEmail,userPassword).addOnCompleteListener(RegistrationActivity.this, task -> {
+                    if(task.isSuccessful()){
+                        Toast.makeText(RegistrationActivity.this, "Successfully Registered", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+                    }
+                    else{
+                        Toast.makeText(RegistrationActivity.this, "Registration Unsuccessful" + task.getException(), Toast.LENGTH_SHORT).show();
+                        Log.e(TAG_ERROR_REGISTER, String.valueOf(task.getException()));
+                    }
+                });
             }
         });
 
-    }
+        try{
+            // clicking on sign in text
+            Log.d(TAG_DEBUG_REGISTER, "Moved to Login Activity");
+            txtsignInRegister.setOnClickListener((View view) -> startActivity(new Intent(RegistrationActivity.this, LoginActivity.class)));
+        }catch(Exception e){
+            Log.e(TAG_ERROR_REGISTER,e.getMessage());
+        }
 
-    public void signIn(View view){
-        startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
     }
+    
 
 
 }
