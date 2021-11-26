2ebffebba656a74236aaceadc9b887f844298b04
Amarpreet Singh <singha163@student.douglascollege.ca>
2021-11-26 11:53:10 -0800

    New login screen created

--- app/src/main/java/com/example/f21g4_minions/MainActivity.java
+++ app/src/main/java/com/example/f21g4_minions/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.f21g4_minions;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button joinNowButton, LoginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        joinNowButton = findViewById(R.id.main_join_now_btn);
+        LoginButton = findViewById(R.id.main_login_btn);
     }
 }
