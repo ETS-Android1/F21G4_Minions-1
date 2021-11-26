807f397736f768700e35db3b36ae991cb5eceadb
Nivid Shah <shahn8@student.douglascollege.ca>
2021-11-26 15:58:07 -0800

    Register Activity finished

--- app/src/main/java/com/example/f21g4_minions/MainActivity.java
+++ app/src/main/java/com/example/f21g4_minions/MainActivity.java
@@ -27,5 +27,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        joinNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
