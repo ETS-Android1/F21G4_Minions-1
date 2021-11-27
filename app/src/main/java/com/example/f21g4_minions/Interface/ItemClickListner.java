8efcca8cc359fdcb85038f4b8db87a7224f31f7a
Arsh Sharma <sharmaa64@student.douglascollege.ca>
2021-11-27 23:13:34 +0530

    Created interface package for adding new interfaces. Added itemclicklistener interface and product view holder

---
+++ app/src/main/java/com/example/f21g4_minions/Interface/ItemClickListner.java
@@ -0,0 +1,8 @@
+package com.example.f21g4_minions.Interface;
+
+import android.view.View;
+
+public interface ItemClickListner {
+
+    void onClick(View view, int position, boolean isLongClick);
+}
