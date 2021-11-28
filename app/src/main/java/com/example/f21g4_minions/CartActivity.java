d31dd69bbbf64d99e7b8d1e49599515206172bfb
Nivid Shah <shahn8@student.douglascollege.ca>
2021-11-28 11:34:08 -0800

    Edit Delete Cart Items

--- app/src/main/java/com/example/f21g4_minions/CartActivity.java
+++ app/src/main/java/com/example/f21g4_minions/CartActivity.java
@@ -1,22 +1,28 @@
 package com.example.f21g4_minions;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.f21g4_minions.Model.Cart;
 import com.example.f21g4_minions.Prevalent.Prevalent;
 import com.example.f21g4_minions.ViewHolder.CartViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -67,6 +73,42 @@ public class CartActivity extends AppCompatActivity {
                 cartViewHolder.txtProductQuantity.setText("Quantity: "+model.getQuantity());
                 cartViewHolder.txtProductName.setText("Product: "+model.getPname());
                 cartViewHolder.txtProductPrice.setText("Price: "+model.getPrice()+"$");
+
+
+                cartViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        CharSequence options[] = new CharSequence[]{
+                          "Edit", "Delete"
+                        };
+                        AlertDialog.Builder builder = new AlertDialog.Builder(CartActivity.this);
+                        builder.setTitle("Cart Options: ");
+
+                        builder.setItems(options, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                if(i==0){
+                                    Intent intent = new Intent(CartActivity.this, ProductDetailsActivity.class);
+                                    intent.putExtra("pid", model.getPid());
+                                    startActivity(intent);
+                                }if(i==1){
+                                    cartListRef.child("User View").child(Prevalent.currentOnlineUser.getPhone()).child("Products")
+                                            .child(model.getPid()).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                Toast.makeText(CartActivity.this, "Item Removed", Toast.LENGTH_SHORT).show();
+                                                startActivity(new Intent(CartActivity.this, HomeActivity.class));
+                                            }
+                                        }
+                                    });
+                                }
+                            }
+                        });
+                        
+                        builder.show();
+                    }
+                });
             }
 
             @NonNull
