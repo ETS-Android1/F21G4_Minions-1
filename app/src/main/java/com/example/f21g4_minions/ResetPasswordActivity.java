be1d514a36d761c7735774253f64957aea92d427
Arsh Sharma <sharmaa64@student.douglascollege.ca>
2021-11-29 19:25:59 +0530

    Designed the password reset layout

--- app/src/main/java/com/example/f21g4_minions/ResetPasswordActivity.java
+++ app/src/main/java/com/example/f21g4_minions/ResetPasswordActivity.java
@@ -6,9 +6,26 @@ import android.os.Bundle;
 
 public class ResetPasswordActivity extends AppCompatActivity {
 
+    private String check = "";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+
+        check = getIntent().getStringExtra("check");
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+
+
+        if(check.equals("settings")){
+
+        }else if(check.equals("login")){
+
+        }
     }
 }
