3bab7f53589ea1532b964af5e2d12d9550031742
Nivid Shah <shahn8@student.douglascollege.ca>
2021-11-30 00:52:30 -0800

    Checkpoint Commit | Android Studio Issue

--- app/src/main/java/com/example/f21g4_minions/Admin/AdminCheckNewProductsActivity.java
+++ app/src/main/java/com/example/f21g4_minions/Admin/AdminCheckNewProductsActivity.java
@@ -1,26 +1,73 @@
 package com.example.f21g4_minions.Admin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.example.f21g4_minions.Model.Products;
 import com.example.f21g4_minions.R;
+import com.example.f21g4_minions.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class AdminCheckNewProductsActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
+    private DatabaseReference unverifiedProductsRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_check_new_products);
 
+        unverifiedProductsRef = FirebaseDatabase.getInstance().getReference().child("Products");
+
+
         recyclerView = findViewById(R.id.admin_products_checklist);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerOptions<Products> options = new FirebaseRecyclerOptions.Builder<Products>()
+                .setQuery(unverifiedProductsRef.orderByChild("productState")
+                        .equalTo("Not Approved"),Products.class)
+                        .build();
+
+        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter = new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ProductViewHolder productViewHolder, int i, @NonNull Products products) {
+                productViewHolder.txtProductName.setText(products.getName());
+                productViewHolder.txtProductDescription.setText(products.getDescription());
+                productViewHolder.txtProductPrice.setText("Price = "+ products.getPrice()+ "$");
+                Picasso.get().load(products.getImage()).into(productViewHolder.imageView);
+
+                final Products itemClick = peo;
+                holder
+
+            }
+
+            @NonNull
+            @Override
+            public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.products_items_layout, parent, false);
+                ProductViewHolder holder = new ProductViewHolder(view);
+                return holder;
+            }
+        };
     }
 }
